b1160bb2881e26fe85e9f9354afcb769b27f61bb
Thomas Desveaux <thomas.desveaux@dont-nod.com>
2023-11-29 14:13:06 +0100

    Setup: Add 'registerRouteWithMenu´ helper

--- src/setup.tsx
+++ src/setup.tsx
@@ -1,21 +1,47 @@
-import {FaCubes} from "react-icons/fa";
-import {buildbotSetupPlugin, RegistrationCallbacks} from "buildbot-plugin-support";
+import {buildbotSetupPlugin, RegistrationCallbacks, RouteConfig, GroupSettings} from "buildbot-plugin-support";
 import {DNEGridView} from './views/GridView/DNEGridView';
 
-buildbotSetupPlugin((reg: RegistrationCallbacks) => {
+type RouteWithMenuConfig = {
+  reg: RegistrationCallbacks,
+} & GroupSettings & RouteConfig;
+
+function registerRouteWithMenu({
+  reg,
+  name,
+  caption,
+  order,
+  route,
+  element,
+  icon,
+  parentName,
+  group,
+}: RouteWithMenuConfig) {
   reg.registerMenuGroup({
-    name: 'dne_grid',
-    caption: 'DNE Grid View',
-    icon: <FaCubes/>,
-    order: 4,
-    route: '/dne_grid',
-    parentName: null,
+    name: name,
+    caption: caption,
+    icon: icon,
+    order: order,
+    route: route,
+    parentName: parentName,
   });
 
   reg.registerRoute({
-    route: "/dne_grid",
-    group: "dne_grid",
+    route: route,
+    group: group ?? name,
+    element: element,
+  });
+}
+
+buildbotSetupPlugin((reg: RegistrationCallbacks) => {
+  registerRouteWithMenu({
+    reg,
+    name: 'dne_grid',
+    caption: 'DNE Grid View',
+    order: 4,
+    route: '/dne_grid',
     element: () => <DNEGridView/>,
+    parentName: null,
+    group: null,
   });
 
   reg.registerSettingGroup({
